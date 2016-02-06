75d67c9f693c3734ddba8cbf3380557741908f2b
gm_at_benincasa <gionata.massi@savoiabenincasa.it>
2016-02-06 10:48:06 +0000

    Esercizio 187, con errori

---
+++ PHP/esercizi_libro/187/func.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>    
+    <body>
+<?php 
+    $numero = $_GET['numero'];
+    for ($div_candidato = 1;
+         $div_candidato <= $numero / 2;
+         $div_candidato++) {
+             if (($numero % $div_candidato) == 0) {
+                 $divisori[] = $div_candidato;
+             }
+    }
+    if (count($divisori) == 1) {
+        echo "Numero primo";
+    } else {
+        echo "<ul>\n";
+        foreach ($divisori as $divisore) {
+            echo "<li>$divisore</li>\n";
+        }
+        echo "</ul>\n";
+    }
+         
+?>
+    </body>
+</html>
