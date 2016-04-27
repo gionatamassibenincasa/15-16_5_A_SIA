258dd365151f0f55cf1222f248962c8d6876139d
gm_at_benincasa <gionata.massi@savoiabenincasa.it>
2016-04-27 08:31:37 +0000

    query

--- esami_di_stato/2007/db-schema.sql
+++ esami_di_stato/2007/db-schema.sql
@@ -56,6 +56,7 @@ DROP TABLE IF EXISTS tipo_prestazione;
 CREATE TABLE IF NOT EXISTS
 tipo_prestazione (
     id INTEGER PRIMARY KEY,
+    nome VARCHAR(50),
     costo REAL,
     tempo_medio INTEGER DEFAULT 15
 );
@@ -73,7 +74,7 @@ prestazione (
     ora_inizio TIME,
     ora_fine TIME DEFAULT NULL,
     costo REAL,
-    effettuato BIT,
+    effettuata BIT,
     UNIQUE (medico, paziente, tipo_prestazione, data)
 );
 
@@ -86,3 +87,149 @@ abilitazione (
     tipo_prestazione INTEGER REFERENCES tipo_prestazione(id),
     PRIMARY KEY (medico, tipo_prestazione)
 );
+
+---
+--- QUERY
+---
+
+-- 1.
+-- elenco giornaliero delle visite prenotate
+-- per ogni singolo medico
+
+DROP VIEW IF EXISTS query_1;
+
+CREATE VIEW IF NOT EXISTS query_1 AS
+select
+    medico.nome AS nome_medico,
+    medico.cognome AS cognome_medico,
+    prestazione.data AS data,
+    prestazione.ora_prenotazione AS ora_prenotazione,
+    paziente.cognome AS cognome_paziente,
+    tipo_prestazione.nome AS nome
+from 
+    medico,
+    prestazione,
+    paziente,
+    tipo_prestazione
+where 
+    medico.cf = prestazione.medico and
+    paziente.cf = prestazione.paziente and
+    prestazione.tipo_prestazione = tipo_prestazione.id and
+    prestazione.effettuata = 0
+order by
+    prestazione.data,
+    medico.cognome,
+    medico.nome;
+
+-- 2.
+-- elenco giornaliero delle visite prenotate e non effettuate
+
+DROP VIEW IF EXISTS query_2;
+CREATE VIEW IF NOT EXISTS query_2 AS
+select
+    medico.nome AS nome_medico,
+    medico.cognome AS cognome_medico,
+    prestazione.data AS data,
+    prestazione.ora_prenotazione AS ora_prenotazione,
+    paziente.cognome AS cognome_paziente,
+    tipo_prestazione.nome AS prestazione
+from (
+        (
+            (medico join prestazione)
+                join paziente
+        )
+                    join tipo_prestazione
+    )
+where (
+    (medico.cf = prestazione.medico) and
+    (paziente.cf = prestazione.paziente) and
+    (prestazione.tipo_prestazione = tipo_prestazione.id) and
+    (prestazione.effettuata = 0) and
+    (prestazione.`data` <= curdate())
+    and (prestazione.ora_prenotazione <= curtime())
+)
+order by
+    prestazione.data,
+    medico.cognome,
+    medico.nome;
+
+-- 3.
+-- elenco settimanale contenente gli appuntamenti di ciascun
+-- medico suddivisi per giorno e per ora
+
+DROP VIEW IF EXISTS query_3;
+CREATE VIEW IF NOT EXISTS query_3 AS
+select week(pr.data,0) AS settimana,
+    (case dayofweek(pr.data)
+        when 1 then 'Lun'
+        when 2 then 'Mar'
+        when 3 then 'Mer'
+        when 4 then 'Gio'
+        when 5 then 'Ven'
+        when 6 then 'Sab'
+        when 7 then 'Dom'
+    end) AS giorno,
+    pr.data AS data,
+    m.cognome AS cognome_medico,
+    m.nome AS nome_medico,
+    t.nome AS nome,
+    pz.cognome AS cognome
+from (
+        (
+            (medico m join prestazione pr)
+                join paziente pz
+        )
+                    join tipo_prestazione t
+    ) 
+where 
+    (m.cf = pr.medico) and
+    (pz.cf = pr.paziente) and
+    (t.id = pr.tipo_prestazione)
+order by
+    week(pr.data,0),
+    dayofweek(pr.data),
+    pr.data,
+    m.cognome,
+    m.nome;
+
+-- 4.
+-- elenco cronologico delle visite usufruite da ciascun paziente
+
+DROP VIEW IF EXISTS query_4;
+CREATE VIEW query_4 AS
+select
+    pz.cognome AS cognome_paziente,
+    pz.nome AS nome_paziente,
+    week(pr.`data`,0) AS settimana,
+    (case dayofweek(pr.`data`)
+        when 1 then 'Lun'
+        when 2 then 'Mar'
+        when 3 then 'Mer'
+        when 4 then 'Gio'
+        when 5 then 'Ven'
+        when 6 then 'Sab'
+        when 7 then 'Dom'
+    end) AS giorno,
+    pr.data AS data,
+    m.cognome AS cognome_medico,
+    m.nome AS nome_medico,
+    t.nome AS nome
+from 
+    (
+        (
+            (medico m join prestazione pr)
+                join paziente pz
+        ) join tipo_prestazione t
+    )
+where (
+    (m.cf = pr.medico) and
+    (pz.cf = pr.paziente) and
+    (t.id = pr.tipo_prestazione) and
+    (pr.effettuata = 1))
+order by
+    pz.cognome,
+    pz.nome,
+    week(pr.data,0),
+    pr.data,
+    m.cognome,
+    m.nome;
